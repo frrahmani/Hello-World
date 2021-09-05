d07dee4487a5aa0334298f0c23720d4144277c17
frrahmani <44092094+frrahmani@users.noreply.github.com>
2021-09-05 10:12:22 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
 print("Hello World")
-print("This message was printed on branch Develop, before merge with master branch")
+print("This message was printed on branch Develop, before merge with master")
 print("added this print with Second Branch 31 August")
 print ("ky shtohet veq per Keten")
