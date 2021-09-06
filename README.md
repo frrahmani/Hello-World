f1fcd479ac3bc6da5fdd7c1a4dcdcb9b44620e36
root <root@ip-172-31-26-255.us-east-2.compute.internal>
2021-09-06 21:45:18 +0000

    edit on EC2 and send on github

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ I will make some changes to python-branch project
 This change has been done on branch Develop!!!!!
 
 added this text from second branch, and delete some data from row 6
+
+Edit and add some text in EC2
--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-print("Hello World")
+print("Hello World - edited on EC2")
 print("This message was printed on branch Develop, before merge with master")
 print("added this print with Second Branch 31 August")
 print ("ky shtohet veq per Keten")
